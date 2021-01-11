bca69651fd5cde82c780a7d3dcaff11011eb08ea
zhenduguqiubai <483580491@qq.com>
2021-01-11 09:58:02 +0800

    提交打开对比视图

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World之git!");
-  System.out.println("林莹先更新");
+        System.out.println("林莹先更新");
         System.out.println("路威后提交");
+
+        System.out.println("提交打开对比视图");
     }
 }
